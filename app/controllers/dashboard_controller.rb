b43df1fe9c159ce0556674018dda9bad0a28971a
Roy Wei <roywjy@gmail.com>
2016-03-03 21:49:21 +0800

    implement searching function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,8 @@ class DashboardController < ApplicationController
   def index
     @strivers = User.paginate(page: params[:page])
   end
+
+  def search
+    @strivers = User.search_by_name(params[:search_name]).paginate(page: params[:page])
+  end
 end
