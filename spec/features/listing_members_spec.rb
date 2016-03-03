15fa159538777185ad5c3935cc46d5dd52637046
Roy Wei <roywjy@gmail.com>
2016-03-03 16:39:57 +0800

    set up listing members testing rspec

---
+++ spec/features/listing_members_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Listing members" do
+
+  before do
+    @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+    @jane = User.create(first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
+  end
+
+  scenario "Show a list of registered memebers" do
+    visit "/"
+
+    expect(page).to have_content("List of Strivers")
+    expect(page).to have_content("@john.full_name")
+    expect(page).to have_content("@jane.full_name")
+  end
+
+end
